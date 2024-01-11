3140c5c4299345918035f8c53fea55528daf359d
Maciej Jaworski <mcjjaworski@gmail.com>
2024-01-11 18:04:24 +0100

    initial commit

---
+++ src/main/java/com/vividious/iot/lepetiteprince/LePetitePrinceApplication.java
@@ -0,0 +1,13 @@
+package com.vividious.iot.lepetiteprince;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LePetitePrinceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LePetitePrinceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/vividious/iot/lepetiteprince/LePetitePrinceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vividious.iot.lepetiteprince;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LePetitePrinceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
